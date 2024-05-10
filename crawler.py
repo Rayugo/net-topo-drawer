43234379f85edb4052316b2116fc7cf34751ad1f
Seggenz <filipwesolowski09@gmail.com>
2024-05-10 17:54:43 +0200

    added tkinter window

--- crawler.py
+++ crawler.py
@@ -32,10 +32,12 @@ self.net_devices = {
 
 """
 
-#czy software-id identyfikuje unikalnie urzadzenie ? czy zostac przy identity?
+# czy software-id identyfikuje unikalnie urzadzenie ? czy zostac przy identity?
 
 
 """Class to collect data about network devices and thier connections"""
+
+
 class Crawler:
     def __init__(self):
         self.net_devices = {}
@@ -47,37 +49,35 @@ class Crawler:
         print(f"Connecting to {net_dev['host']} ...")
         self.curr_net_dev = net_dev
         net_connect = ConnectHandler(**net_dev)
-        neighbour = net_connect.send_command("ip neighbor/print detail") #here need to detect what device we are trying to connect to (cisco, huawei ...)
+        neighbour = net_connect.send_command(
+            "ip neighbor/print detail")  # here need to detect what device we are trying to connect to (cisco,
+        # huawei ...)
         identity = net_connect.send_command("system identity/print")
-        neighbour_splited = neighbour.split('\n')
-        neighbour_splited = list(filter(None, neighbour_splited))
-        identity = re.search(self.pattern_identity,identity)
+        neighbour_splitted = neighbour.split('\n')
+        neighbour_splitted = list(filter(None, neighbour_splitted))
+        identity = re.search(self.pattern_identity, identity)
         identity = identity.group(1)
-        
-        #adding found connections to neighbouring net devices
-        self.net_devices[identity] = {'connections': {} }
-        for c,i in enumerate(neighbour_splited):
+
+        # adding found connections to neighbouring net devices
+        self.net_devices[identity] = {'connections': {}}
+        for c, i in enumerate(neighbour_splitted):
             matches = re.findall(self.pattern_neighbour, i)
             found_con = {key: value.strip('"') for key, value in matches}
             self.net_devices[identity]['connections'][c] = found_con
 
-        curr_net_devices = self.net_devices.copy() #need to do copy because of recursive search
-        
-        #trying to connect to neighbouring devices that do not exist in self.net_devices
+        curr_net_devices = self.net_devices.copy()  # need to do copy because of recursive search
+
+        # trying to connect to neighbouring devices that do not exist in self.net_devices
         for net_dev in curr_net_devices.values():
             for con in net_dev['connections'].values():
                 if not self._exists(con['identity']) and 'address' in con.keys():
-                    new_net_dev = {
-                        'device_type': 'mikrotik_routeros',
-                        'username': 'admin',
-                        'password': 'admin',
-                    }
-                    new_net_dev['host'] = con['address']
-                    self.run_crawler(new_net_dev) #start recursive search until no new devices found
-        
-    def _exists(self, found_con_idenity):
+                    new_net_dev = {'device_type': 'mikrotik_routeros', 'username': 'admin', 'password': 'admin',
+                                   'host': con['address']}
+                    self.run_crawler(new_net_dev)  # start recursive search until no new devices found
+
+    def _exists(self, found_con_identity):
         for identity in self.net_devices.keys():
-            if identity == found_con_idenity:
+            if identity == found_con_identity:
                 return True
         return False
 
--- main.py
+++ main.py
@@ -1,18 +1,50 @@
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+import networkx as nx
+import ast
+import numpy as np
 
-from netmiko import ConnectHandler
-from crawler import Crawler
+from netgraph import InteractiveGraph
 
-mikrotik_1 = {
-    'device_type': 'mikrotik_routeros',
-    'host': '192.168.126.130',
-    'username': 'admin',
-    'password': 'admin',
-}
+
+def draw_graph(canvas):
+    with open('test_data.txt', 'r') as test_file:
+        content = test_file.read()
+        data = ast.literal_eval(content)
+
+    # Tworzenie grafu
+    G = nx.Graph()
+
+    # Dodawanie węzłów i krawędzi do grafu
+    for router, connections in data.items():
+        G.add_node(router)
+        for connection in connections['connections'].values():
+            G.add_edge(router, connection['identity'])
+
+    # Rysowanie grafu
+    pos = nx.spring_layout(G)  # układ wiosenny
+    nx.draw(G, pos, with_labels=True, node_size=1500, node_color='skyblue', font_size=10, font_weight='bold', width=2)
+
 
 def main():
-    c = Crawler()
-    c.run_crawler(mikrotik_1)
-    c.print_net_devices()
+    root = tk.Tk()
+    root.title("Net-Topology Drawer")
+
+    # Kontener dla wykresu
+    frame = tk.Frame(root)
+    frame.pack()
+
+    # Tworzenie wykresu
+    fig = plt.figure(figsize=(8, 6))
+    canvas = FigureCanvasTkAgg(fig, master=frame)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    draw_graph(canvas)
+
+    root.mainloop()
+
 
 if __name__ == "__main__":
     main()
