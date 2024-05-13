f7bad51ae9d97d77b5e040bc57352e2f31ce26ee
Rayugo <jan.szolc@gmail.com>
2024-05-13 21:07:14 +0200

    Code refactored and prepared for device type recognition.

--- crawler.py
+++ crawler.py
@@ -36,6 +36,8 @@ self.net_devices = {
 
 
 """Class to collect data about network devices and thier connections"""
+
+
 class Crawler:
     def __init__(self):
         self.net_devices = {}
@@ -46,40 +48,44 @@ class Crawler:
     def run_crawler(self, net_dev):
         print(f"Connecting to {net_dev['host']} ...")
         self.curr_net_dev = net_dev
-        net_connect = ConnectHandler(**net_dev)
-        neighbour = net_connect.send_command("ip neighbor/print detail") #here need to detect what device we are trying to connect to (cisco, huawei ...)
-        identity = net_connect.send_command("system identity/print")
-        neighbour_splited = neighbour.split('\n')
-        neighbour_splited = list(filter(None, neighbour_splited))
-        identity = re.search(self.pattern_identity,identity)
-        identity = identity.group(1)
-        
-        #adding found connections to neighbouring net devices
-        self.net_devices[identity] = {'connections': {} }
-        for c,i in enumerate(neighbour_splited):
-            matches = re.findall(self.pattern_neighbour, i)
-            found_con = {key: value.strip('"') for key, value in matches}
-            self.net_devices[identity]['connections'][c] = found_con
+        self._get_device_data(self.curr_net_dev['device_type'])
 
-        curr_net_devices = self.net_devices.copy() #need to do copy because of recursive search
-        
+        curr_net_devices = self.net_devices.copy()  #need to do copy because of recursive search
+
         #trying to connect to neighbouring devices that do not exist in self.net_devices
         for net_dev in curr_net_devices.values():
             for con in net_dev['connections'].values():
-                if not self._exists(con['identity']) and 'address' in con.keys():
-                    new_net_dev = {
-                        'device_type': 'mikrotik_routeros',
-                        'username': 'admin',
-                        'password': 'admin',
-                    }
-                    new_net_dev['host'] = con['address']
-                    self.run_crawler(new_net_dev) #start recursive search until no new devices found
-        
-    def _exists(self, found_con_idenity):
+                if not self._exists(con['identity']) and 'address' in con.keys():  #need to correct condition to match different products
+                    self.run_crawler(self._create_new_net_dev(con))  #start recursive search until no new devices found
+
+    def _exists(self, found_con_identity):
         for identity in self.net_devices.keys():
-            if identity == found_con_idenity:
+            if identity == found_con_identity:
                 return True
         return False
 
     def print_net_devices(self):
         pprint(self.net_devices)
+
+    def _get_device_data(self, device_type):
+        net_connect = ConnectHandler(**self.curr_net_dev)
+        if device_type == 'mikrotik_routeros':
+            neighbour = net_connect.send_command(
+                "ip neighbor/print detail")  #here need to detect what device we are trying to connect to (cisco, huawei ...)
+            identity = net_connect.send_command("system identity/print")
+            neighbour_split = neighbour.split('\n')
+            neighbour_split = list(filter(None, neighbour_split))
+            identity = re.search(self.pattern_identity, identity)
+            identity = identity.group(1)
+
+            #adding found connections to neighbouring net devices
+            self.net_devices[identity] = {'connections': {}}
+            for c, i in enumerate(neighbour_split):
+                matches = re.findall(self.pattern_neighbour, i)
+                found_con = {key: value.strip('"') for key, value in matches}
+                self.net_devices[identity]['connections'][c] = found_con
+
+    def _create_new_net_dev(self, con):
+        new_net_dev = {'device_type': 'mikrotik_routeros', 'username': 'admin', 'password': 'admin',
+                       'host': con['address']}
+        return new_net_dev
