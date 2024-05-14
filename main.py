b485cf5be9d2a169a97c2efd7265907bf26eda1a
Seggenz <filipwesolowski09@gmail.com>
2024-05-14 16:45:07 +0200

    adjusted stackoverflow code to our test_file, adjusted labels

--- main.py
+++ main.py
@@ -1,50 +1,129 @@
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
-import ast
-import numpy as np
 
 from netgraph import InteractiveGraph
 
 
-def draw_graph(canvas):
+def draw_graph():
+    g = nx.Graph()
+
     with open('test_data.txt', 'r') as test_file:
         content = test_file.read()
         data = ast.literal_eval(content)
 
-    # Tworzenie grafu
-    G = nx.Graph()
-
-    # Dodawanie węzłów i krawędzi do grafu
     for router, connections in data.items():
-        G.add_node(router)
+        g.add_node(router)
         for connection in connections['connections'].values():
-            G.add_edge(router, connection['identity'])
+            g.add_edge(router, connection['identity'])
 
-    # Rysowanie grafu
-    pos = nx.spring_layout(G)  # układ wiosenny
-    nx.draw(G, pos, with_labels=True, node_size=1500, node_color='skyblue', font_size=10, font_weight='bold', width=2)
+    edge_color = dict()
+    for ii, edge in enumerate(g.edges):
+        edge_color[edge] = 'tab:gray' if ii % 2 else 'tab:orange'
 
+    node_color = dict()
+    for node in g.nodes:
+        if node:
+            num = int(node[1:])
+        node_color[node] = 'tab:red' if num % 2 else 'tab:blue'
 
-def main():
-    root = tk.Tk()
-    root.title("Net-Topology Drawer")
+    fig, ax = plt.subplots()
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    ax.patch.set_edgecolor('black')
+    ax.patch.set_linewidth(1)
+    ax.autoscale_view(True, True, True)
+    fig.set_size_inches(16, 9)
+    plot_instance = InteractiveGraph(
+        g, node_size=5, node_color=node_color,
+        node_labels=True, node_label_offset=0.1, node_label_fontdict=dict(size=20),
+        edge_color=edge_color, edge_width=1, ax=ax, scale=(2, 2))
 
-    # Kontener dla wykresu
-    frame = tk.Frame(root)
-    frame.pack()
+    """
+    interface_labels = [
+        ((1, 2), 0.2, 'eth0'),
+        ((1, 2), 0.8, 'eth1'),
+        ((2, 3), 0.2, 'gig0'),
+        ((2, 3), 0.8, 'gig0'),
+    ]
+    """
+    interface_labels = []
+    edges = []
 
-    # Tworzenie wykresu
-    fig = plt.figure(figsize=(8, 6))
-    canvas = FigureCanvasTkAgg(fig, master=frame)
+    # [[R1, R2], , ],
+    # [[R1, R2], , ],
+    # [[R2, R3], , ],
+    # ...
+
+    for router, connections in data.items():
+        for connection in connections['connections'].values():
+            if [connection['identity'], router] in edges:
+                continue
+
+            edges.append([router, connection['identity']])
+
+            try:
+                interface_labels.append([[router, connection['identity']], 0.2, connection['interface']])
+                interface_labels.append([[router, connection['identity']], 0.8, connection['interface-name']])
+            except KeyError:
+                print(f"Nie dodano interfejsu dla krawedzi {router}-{connection['identity']}")
+
+    print(f"Zarejestrowane etykiety: {interface_labels}")
+
+    def get_label_position(edge, edge_label_position, node_positions):
+        v1, v2 = edge
+        dxy = node_positions[v2] - node_positions[v1]
+        xy = edge_label_position * dxy + node_positions[v1]
+        angle = np.arctan2(dxy[1], dxy[0]) * 360 / (2.0 * np.pi)
+
+        # reduce overlap with nodes
+        if 90 <= (angle % 360) < 270:
+            if edge_label_position < 0.5:
+                horizontal_alignment = "right"
+            else:
+                horizontal_alignment = "left"
+        else:
+            if edge_label_position < 0.5:
+                horizontal_alignment = "left"
+            else:
+                horizontal_alignment = "right"
+
+        # make label orientation "right-side-up"
+        if angle > 90:
+            angle -= 180
+        if angle < - 90:
+            angle += 180
+
+        return xy, angle, horizontal_alignment
+
+    interface_label_text_objects = []
+    for edge, edge_label_position, label in interface_labels:
+        (x, y), angle, ha = get_label_position(edge, edge_label_position, plot_instance.node_positions)
+        text_object = ax.text(x, y, label, rotation=angle, rotation_mode="anchor", va="center", ha=ha)
+        interface_label_text_objects.append(text_object)
+
+    def update_label_positions(event):
+        for (edge, edge_label_position, _), text_object in zip(interface_labels, interface_label_text_objects):
+            xy, angle, ha = get_label_position(
+                edge, edge_label_position, plot_instance.node_positions)
+            text_object.set_position(xy)
+            text_object.set_rotation(angle)
+            text_object.set_horizontalalignment(ha)
+
+    fig.canvas.mpl_connect('button_release_event', update_label_positions)
+
+    canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-    draw_graph(canvas)
 
-    root.mainloop()
+root = tk.Tk()
+root.title("Net Topology Drawer")
+root.minsize(width=1280, height=720)
+frame = tk.Frame(root)
+frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
+draw_graph()
 
-if __name__ == "__main__":
-    main()
+root.mainloop()
